1ba2a6c9c673e996377137972b11373733ffbdad
doubtfulcoder <naveedtkabir@gmail.com>
2022-07-14 23:01:47 -0400

    Fixed predict method to actually predict

--- knn_levenshtein.py
+++ knn_levenshtein.py
@@ -54,21 +54,36 @@ class KNearestLevenshtein:
         neighbors = KNearestLevenshtein.get_neighbors(
             self.species, other_species, 4)
         print('neighbors', neighbors)
-        return neighbors[0]
+
+        # Make dictionary of predictions and # of times they appear
+        predictions = {}
+        for neighbor in neighbors:
+            if neighbor[1] in predictions:
+                predictions[neighbor[1]] += 1
+            else:
+                predictions[neighbor[1]] = 1
+        print('preds', predictions)
+
+        # Find most common prediction
+        max_prediction = max(predictions, key=predictions.get)
+        print('max prediction', max_prediction)
+
+        return max_prediction
 
     # Gets the k most similar neighbors using levenshtein distance
     def get_neighbors(correct_seq, other_species, k):
         neighbors = []
 
         # Loop over other species genome 4 characters at a time
-        for i in range(0, len(other_species)-20, 4):
+        for i in range(0, len(other_species)-24, 4):
             # Check every substring of 20 chars in other_species
             substring = other_species[i:i+20]
-            neighbors.append((substring,
+            prediction = other_species[i+20:i+24]
+            neighbors.append((substring, prediction,
                               KNearestLevenshtein.levenshteinDistanceCalc(substring, correct_seq)))
 
         # Sort neighbors by distance
-        neighbors.sort(key=lambda tup: tup[1])
+        neighbors.sort(key=lambda tup: tup[2])
 
         return neighbors[:k]
 
