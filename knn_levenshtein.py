48f8b9a6afa2c4528113c598e745608644a95ea0
doubtfulcoder <naveedtkabir@gmail.com>
2022-07-18 20:27:10 -0400

    Redirected output to message.log and fixed prediction and substring_pre/post

--- knn_levenshtein.py
+++ knn_levenshtein.py
@@ -1,22 +1,26 @@
 import random
+import sys
 
 import numpy as np
 from Levenshtein import distance
 
+# Redirect output to file for easier viewing
+log_file = open("message.log", "w")
+sys.stdout = log_file
+
+
 # Opens chromosome file
-
-
 def open_file(file_name, character_limit=10000):
     with open(file_name, 'r') as f:
         return f.readlines(character_limit)[1:]
 
 
 # Hides random sequences of bases in the genome
-def hide_random_sequence(genome, max_length=6):
-    iters = int(len(genome) / 70)  # number of times we should hide a sequence
+def hide_random_sequence(genome, max_length=10):
+    iters = int(len(genome) / 100)  # number of times we should hide a sequence
     for i in range(iters):
         # hide sequences of random length from 1 to max_length
-        hidden_sequence_length = random.randint(1, max_length)
+        hidden_sequence_length = random.randint(5, max_length)
         # pick a random position and hide sequence starting from there
         start_index = random.randint(0, len(genome) - hidden_sequence_length)
         new_genome = genome[:start_index]
@@ -47,7 +51,7 @@ def missing_values_array(genome, new_genome):
 
 
 class KNearestLevenshtein:
-    def __init__(self, s_range=1000, area=10, max_ld=3, k=5):
+    def __init__(self, s_range=1000, area=20, max_ld=3, k=10):
         # defines how far back and forward the model will look from the location of the missing values for imputation
         self.search_range = s_range
 
@@ -62,7 +66,7 @@ class KNearestLevenshtein:
 
     # the function that will return a 2D array having levenshtein distances between respective indices of string 1 and 2.
     def levenshteinDistanceCalc(token1, token2):
-        return distance(token1, token2, score_cutoff=12)
+        return distance(token1, token2)
         distances = np.zeros((len(token1) + 1, len(token2) + 1))
 
         for t1 in range(len(token1) + 1):
@@ -94,7 +98,7 @@ class KNearestLevenshtein:
         # return the levenshtein distance
         return distances[len(token1)][len(token2)]
 
-    # Fits model to a sequence in genome with next 4 characters to be predicted
+    # Fits model to a sequence
     def fit(self, species):
         # later to be changed into a for loop in order to account for multiple species
         self.species = species
@@ -114,17 +118,19 @@ class KNearestLevenshtein:
                         break
                 neighbors = KNearestLevenshtein.get_neighbors(
                     self, self.species, other_species, blank_len, blank_idx)
-                # print('neighbors for blank space at ', i, 'are: ', neighbors)
+                print('neighbors for blank space at ', i, 'are: ', neighbors)
 
                 # Make dictionary of predictions and # of times they appear
                 predictions = {}
+                total_levenshtein = 0
                 for neighbor in neighbors:
+                    total_levenshtein += neighbor[1]
                     if neighbor[0] in predictions:
                         predictions[neighbor[0]] += 1
                     else:
                         predictions[neighbor[0]] = 1
                 #print('preds', predictions)
-
+                # print(total_levenshtein / len(neighbors))
                 # Find most common prediction
                 max_prediction = max(predictions, key=predictions.get)
                 actual_preds.append(max_prediction)
@@ -142,10 +148,10 @@ class KNearestLevenshtein:
             substring_pre = other_species[i:i + self.search_area]
             substring_post = other_species[i + self.search_area +
                                            blank_len:i + 2*(self.search_area)+blank_len]
-            prediction = other_species[i + self.search_area +
-                                       1:i + self.search_area + 1 + blank_len]
+            prediction = other_species[i +
+                                       self.search_area:i + self.search_area + blank_len]
             neighbors.append((prediction, (KNearestLevenshtein.levenshteinDistanceCalc(substring_pre, correct_seq[blank_idx - self.search_area - 1: blank_idx]) + KNearestLevenshtein.levenshteinDistanceCalc(
-                substring_pre, correct_seq[blank_idx + blank_len: blank_idx + blank_len + self.search_area]))/2))
+                substring_post, correct_seq[blank_idx + blank_len: blank_idx + blank_len + self.search_area]))/2))
 
         # Sort neighbors by distance
         neighbors.sort(key=lambda tup: tup[1])
@@ -158,24 +164,32 @@ char_limit = 50000
 genomeList = open_file('chrI_celegans.fna', character_limit=char_limit)
 genome = ''.join(genomeList)  # combine each line of genome into 1 string
 genome = genome.replace('\n', '')  # remove newline characters
-genome_missing = genome[:int(char_limit/2)]
-genome_full = genome[int(char_limit/2):]
-otherGenomeList = open_file('chrI_cbriggsae.fna')
+
+# genome_missing = genome[1200:char_limit]
+# genome_full = genome[1200:char_limit]
+
+genome_missing = genome
+
+# genome_missing = genome[:int(len(genome)/2)]
+# genome_full = genome[int(len(genome)/2):]
+# genome_full = genome[:int(char_limit/2)]
+otherGenomeList = open_file('chrI_cbriggsae.fna', character_limit=char_limit)
 # combine each line of genome into 1 string
 otherGenome = ''.join(otherGenomeList)
-otherGenome = otherGenome.replace('\n', ' ')  # remove newline characters
+otherGenome = otherGenome.replace('\n', '')  # remove newline characters
 new_genome = hide_random_sequence(genome_missing)
 correct_values = missing_values_array(genome_missing, new_genome)
-# print(otherGenome, '\n')
-# print(new_genome, '\n')
+# print(len(otherGenome), '\n')
+# print(len(new_genome), '\n')
 # print(correct_values)
 
 kn = KNearestLevenshtein()
 kn.fit(new_genome)
-preds = kn.predict(genome_full)
+# preds = kn.predict(genome_full)
+preds = kn.predict(otherGenome)
 
-print(correct_values, '\n')
-print(preds)
+print('correct values:', correct_values, '\n')
+print('predictions:', preds)
 
 num_correct = 0
 num_total = 0
@@ -190,7 +204,7 @@ for i in range(len(correct_values)):
             num_correct += 1
         num_total += 1
 print('Accuracy: ', num_correct/num_total)
-print('Levnshtein Error: ', np.mean(error_rates))
+print('Levenshtein Error: ', np.mean(error_rates))
 
 # tests
 # print(KNearestLevenshtein.levenshteinDistanceCalc("hello", "hello"))
